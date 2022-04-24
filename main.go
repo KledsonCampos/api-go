2fecdea28b972b841ccc4223d049f9ebc0645343
KledsonCampos <kledson.ti@gmail.com>
2022-04-24 16:21:43 -0300

    finished the api documentation

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
@@ -14,9 +15,9 @@ import (
 // Event model info
 // @Description Event information about the event
 type Event struct {
-	Id          uuid.UUID `json:"uuid" swaggertype:"uuid"`
-	Title       string    `json:"title" swaggertype:"string"`
-	Description string    `json:"description" swaggertype:"string"`
+	Id          uuid.UUID `json:"uuid"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
 }
 
 type allEvents []Event
@@ -36,20 +37,24 @@ var events = allEvents{}
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 
 // @host      https://api-go-events.herokuapp.com/
+// @BasePath /
 
 func main() {
 	log.Println("Starting API")
 	router := mux.NewRouter()
-	router.HandleFunc("/", Home)
-	router.HandleFunc("/health-check", HealthCheck).Methods("GET")
-	router.HandleFunc("/events", GetAllEvents).Methods("GET")
-	router.HandleFunc("/events", CreateNewEvent).Methods("POST")
-	// port := os.Getenv("PORT")
-	// http.ListenAndServe(":"+port, router)
-	http.ListenAndServe(":8086", router)
+	router.HandleFunc("/", home)
+	router.HandleFunc("/health-check", healthCheck).Methods("GET")
+	router.HandleFunc("/event/{id}", getOneEvent).Methods("GET")
+	router.HandleFunc("/event", getAllEvents).Methods("GET")
+	router.HandleFunc("/event", createNewEvent).Methods("POST")
+	router.HandleFunc("/event/{id}", updateEvent).Methods("PUT")
+	router.HandleFunc("/event/{id}", deleteEvent).Methods("DELETE")
+
+	port := os.Getenv("PORT")
+	http.ListenAndServe(":"+port, router)
+	//http.ListenAndServe(":8086", router)
 }
 
-// ShowAllEvents godoc
 // @Summary      Show all events
 // @Description  List all events
 // @Tags         events
@@ -59,25 +64,27 @@ func main() {
 // @Success      200  {object}  Event
 
 // @Router       /events [get]
-func GetAllEvents(w http.ResponseWriter, r *http.Request) {
-	log.Println("Acesando o endpoint get all events")
+// GetAllEvents
+func getAllEvents(w http.ResponseWriter, r *http.Request) {
+	log.Println("Acessando o endpoint get all event")
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(events)
 }
 
-func HealthCheck(w http.ResponseWriter, r *http.Request) {
+// HealthCheck
+func healthCheck(w http.ResponseWriter, r *http.Request) {
 	log.Println("Acessando health-check")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Aplicação em execução!\n")
 }
 
-func Home(w http.ResponseWriter, r *http.Request) {
+// Home
+func home(w http.ResponseWriter, r *http.Request) {
 	log.Println("Acessando home")
 	fmt.Fprintf(w, "HOME!\n")
 }
 
-// CreateEvent godoc
 // @Summary      Create new event
 // @Description  Create new event
 // @Tags         events
@@ -87,7 +94,8 @@ func Home(w http.ResponseWriter, r *http.Request) {
 // @Success      201  {object}  Event
 
 // @Router       /events [post]
-func CreateNewEvent(w http.ResponseWriter, r *http.Request) {
+// createNewEvent
+func createNewEvent(w http.ResponseWriter, r *http.Request) {
 	log.Println("Acessando create new event")
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
@@ -106,4 +114,66 @@ func CreateNewEvent(w http.ResponseWriter, r *http.Request) {
 	events = append(events, newEvent)
 
 	json.NewEncoder(w).Encode(events)
+}
+
+// @Summary Get a Event by ID
+// @ID get-one-event
+// @Produce json
+// @Param id path string true "event ID"
+// @Success 200 {object} todo
+// @Failure 404 {object} message
+// @Router /evetnt/{id} [get]
+// getOneEvent
+func getOneEvent(w http.ResponseWriter, r *http.Request) {
+	log.Println("Get one event")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	eventID := mux.Vars(r)["id"]
+
+	for _, singleEvent := range events {
+		if singleEvent.Id.String() == eventID {
+			json.NewEncoder(w).Encode(singleEvent)
+		}
+	}
+}
+
+// updateEvent
+func updateEvent(w http.ResponseWriter, r *http.Request) {
+	log.Println("Atualizando event")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	eventID := mux.Vars(r)["id"]
+	var updatedEvent Event
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	json.Unmarshal(reqBody, &updatedEvent)
+
+	for i, singleEvent := range events {
+		if singleEvent.Id.String() == eventID {
+			singleEvent.Title = updatedEvent.Title
+			singleEvent.Description = updatedEvent.Description
+			events = append(events[:i], singleEvent)
+			json.NewEncoder(w).Encode(singleEvent)
+		}
+	}
+}
+
+func deleteEvent(w http.ResponseWriter, r *http.Request) {
+	log.Println("Deletando event")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	eventID := mux.Vars(r)["id"]
+
+	for i, singleEvent := range events {
+		if singleEvent.Id.String() == eventID {
+			events = append(events[:i], events[i+1:]...)
+			log.Printf("The event with ID %v has been deleted successfully", eventID)
+		}
+	}
 }
